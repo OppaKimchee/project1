e546020bc55d18c67d71110f2b1c22724bb122cc
Justin Kimn <djkimchee@gmail.com>
2017-04-03 09:05:19 -0700

    finished a chunk of player logic

--- js/functions.js
+++ js/functions.js
@@ -1,4 +1,69 @@
-//Random Number Generator to pick cards from 0 - 51 (array index)
-function ranNumGen(){
-  return Math.floor(Math.random() * 51)
+/*-- Random Number Generator to pick cards from 0 - 51 (array index) --*/
+function ranNumGen(n){
+  return Math.floor(Math.random() * n);
+}
+
+/*-- Shuffle --*/
+function shuffle(array) {
+  var c = array.length, t, r;
+
+  // While there remain elements to shuffle…
+  while (c) {
+    // Pick a remaining element…
+    r = Math.floor(Math.random() * c--);
+    // And swap it with the current element.
+    t = array[c];
+    array[c] = array[r];
+    array[r] = t;
+  }
+  return array;
+}
+
+/*-- Ace Promp --*/
+function ace(hand){
+  var aces = ["H1", "D1", "S1", "C1"];
+  if(hand === playerHand){
+    for (var i = 0; i < aces.length; i++){
+      if(playerHand.includes(aces[i])){
+        alert("Ace: would u like a 1 or 11?");
+      }
+    }
+  }
+  return;
+}
+
+/*-- deal card to player depending on passed value --*/
+function deal(player){
+  if(player === playerHand){
+    playerHand.push(shuffledDeck[0]);
+    shuffledDeck.splice(0,1);
+  }
+  else{
+    houseHand.push(shuffledDeck[0])
+    shuffledDeck.splice(0,1);
+  }
+}
+
+/*-- check for bust or not --*/
+function check(hand, val){
+  for(var i = 0; i < hand.length; i++){
+    var numVal = parseInt(hand[i].substring(1,hand.length));
+    switch (numVal){
+      case 1: ace(hand); break;
+      case 2: val += numVal; break;
+      case 3: val += numVal; break;
+      case 4: val += numVal; break;
+      case 5: val += numVal; break;
+      case 6: val += numVal; break;
+      case 7: val += numVal; break;
+      case 8: val += numVal; break;
+      case 9: val += numVal; break;
+      case 10: val += numVal; break;
+      case 11: val += 10; break;
+      case 12: val += 10; break;
+      case 13: val += 10; break;
+      default:  break;
+    }
+  }
+  return player > 21 ? bust = true : bust = false;
 }
--- js/script.js
+++ js/script.js
@@ -6,6 +6,8 @@ var playerHand;
 var houseHand;
 var playerVal;
 var houseVal;
+var hit;
+var bust;
 var bank;
 var bet;
 var pool;
@@ -14,21 +16,61 @@ var pool;
 function init(){
   /*--!!!!remeber 1 is ACE which can equal to 1 or 11!!!!!--*/
   deck = [
-    { 1 : 'H', 2 : 'H', 3 : 'H', 4 : 'H', 5 : 'H', 6 : 'H',  7 : 'H', 8 : 'H', 9 : 'H', 10 : 'H', 11 : 'H', 12 : 'H', 13 : 'H'},
-    { 1 : 'D', 2 : 'D', 3 : 'D', 4 : 'D', 5 : 'D', 6 : 'D',  7 : 'D', 8 : 'D', 9 : 'D', 10 : 'D', 11 : 'D', 12 : 'D', 13 : 'D'},
-    { 1 : 'S', 2 : 'S', 3 : 'S', 4 : 'S', 5 : 'S', 6 : 'S',  7 : 'S', 8 : 'S', 9 : 'S', 10 : 'S', 11 : 'S', 12 : 'S', 13 : 'S'},
-    { 1 : 'C', 2 : 'C', 3 : 'C', 4 : 'C', 5 : 'C', 6 : 'C',  7 : 'C', 8 : 'C', 9 : 'C', 10 : 'C', 11 : 'C', 12 : 'C', 13 : 'C'},
-  ];
-  shuffledDeck = {};
-  playerHand = {}
-  houseHand = {}
+    "H1","H2","H3","H4","H5","H6","H7","H8","H9","H10","H11","H12","H13",
+    "D1","D2","D3","D4","D5","D6","D7","D8","D9","D10","D11","D12","D13",
+    "S1","S2","S3","S4","S5","S6","S7","S8","S9","S10","S11","S12","S13",
+    "C1","C2","C3","C4","C5","C6","C7","C8","C9","C10","C11","C12","C13"
+  ]
+  shuffledDeck = [];
+  playerHand = [];
+  houseHand = [];
   playerVal = 0;
   houseVal = 0;
+  hit = false;
+  bust = false;
   bank = 1000;
   bet = 0;
   pool = 0;
 }
 
 /*-- Game Logic --*/
+function start(){
+  //shuffle deck on game start
+  shuffledDeck = shuffle(deck);
+  //bets
+  //show playerHand[0] on board
+  deal(playerHand);
+  //hide houseHand[0] on board
+  deal(houseHand);
+  //show playerHand[1] on board
+  deal(playerHand);
+  //show houseHand[1] on board
+  deal(houseHand);
+
+  while(bust === false){
+      check(playerHand, playerVal);
+      check(houseHand, houseVal);
+      ace(playerHand);
+      ace(houseHand);
+      bust = true;
+    }
+    //would u like to stay or hit
+    if(hit === true){
+      deal(playerHand);
+    }
+    else{
+      deal(houseHand);
+    }
+  }
 
 
+//******careful of ace = 1 or 11*********
+//playerVal = card1 + card2
+//houseVal = card1 + card2 ***only show one card***
+//check is 21? no? then ask player to hit or stand
+//
+
+
+
+init();
+start();
